2713f31a52d0492735affe42e0bf2f6fed2e8c2a
GabrielyMachado <gabriely.machado@hotmail.com>
2023-04-09 11:14:34 -0300

    feat: definindo estrutura inicial

---
+++ lib/components/category_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(category.title),
+    );
+  }
+}
