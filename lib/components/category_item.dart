9f358ceaad53c930c0d092769aa27a9f8c844769
GabrielyMachado <gabriely.machado@hotmail.com>
2023-04-09 21:01:03 -0300

    feat: criando rota de teste

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,35 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category, {super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriesMealsScreen(category);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        image: DecorationImage(
-          image: AssetImage(category.image),
-          fit: BoxFit.cover,
-          opacity: 0.4,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+            image: AssetImage(category.image),
+            fit: BoxFit.cover,
+            opacity: 0.4,
+          ),
         ),
-        // gradient: LinearGradient(
-        //   colors: [
-        //     category.color.withOpacity(0.5),
-        //     category.color,
-        //   ],
-        //   begin: Alignment.topLeft,
-        //   end: Alignment.bottomRight,
-        // ),
-      ),
-      child: Center(
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleMedium,
+        child: Center(
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
         ),
       ),
     );
